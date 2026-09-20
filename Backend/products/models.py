d2eb6f5a919bb77548985a8a7910370a5e294fc0
HamidReza Bababeyk <HR.bababeyk@gmail.com>
2026-09-20 21:03:51 +0330

    commit-2

--- Backend/products/models.py
+++ Backend/products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(_max_length=100)
+    slug=models.SlugField(max_length=120,unique=True )
+
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category= models.ForeignKey(
+        Category,
+        on_delete = models.PROTECT,
+        related_name='products'    
+    )    
+
+
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=220, unique=True) 
+
+
+    description = models.BooleanField(blank=True)
+
+    material=models.CharField(max_length =100,blank=True)
+
+    is_active=models.BooleanField(default=True)
+    create_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
